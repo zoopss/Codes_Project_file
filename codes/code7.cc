3e040c76d5d6044b206ab2f7974396505ee1f2ac
Pranjal Kulshreshth <zoopss@users.noreply.github.com>
2017-01-21 20:42:05 +0530

    another one

--- codes/code7.cc
+++ codes/code7.cc
@@ -1,1 +1,20 @@
-
+#include<iostream.h>
+#include<fstream.h>
+#include<stdlib.h>
+int main()
+{   ofstream fout;
+    fout.open("marks.dat",los::app);
+    char ans = 'y';
+    int rollno;
+    float marks;
+    system(" cls ");
+    while(ans =='y' ||ans == 'Y')
+    { cout<<"\n Enter Rollno.";
+      cin>>Rollno;
+      fout<<rollno<<'\n'<<marks<<'\n';
+      cout<<"\n Want to enter more records?(y/n)........";
+      cin>>ans;
+    }
+    fout.close();
+    return 0;
+}
