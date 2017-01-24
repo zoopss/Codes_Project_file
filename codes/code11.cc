1225be6a0d4f73153f099dc1649d12080a850730
Pranjal Kulshreshth <zoopss@users.noreply.github.com>
2017-01-24 10:48:23 +0530

    updated

--- codes/code11.cc
+++ codes/code11.cc
@@ -1,38 +1,42 @@
 #include<iostream>
-#include<process.h>
  
 using namespace std;
  
 int main()
 {
     int a[50],x,n,i,j,b[50];
-    cout<<"How many elements of Array you want to create?";
+    cout<<"How many elements do you want to create array with?(max. 50)....";
     cin>>n;
-    cout<<"\nEnter elements of Array\n";
+    cout<<"\nEnter Array elements...\n";
     
     for(i=0;i<n;++i)
         cin>>a[i];
-        
-    cout<<"\nEnter element to delete:";
-    cin>>x;
+    char ch='y';
+    while(ch=='y'||ch =='Y')
+    {
+     cout<<"\nEnter element to be deleted:";
+     cin>>x;
     
-    for(i=0,j=0;i<n;++i)
-    {
-        if(a[i]!=x)
-            b[j++]=a[i];
+     for(i=0,j=0;i<n;++i)
+     {
+         if(a[i]!=x)
+             b[j++]=a[i];
+     }
+     cout<<"Wnat to Delete more elements(y/n)?...";
+     cin>>ch;
     }
+     if(j==n)
+     {
+         cout<<"\nSoory!!!Element is not in the Array";
+         exit(0);
+     }
+     else
+     {
+         cout<<"\nNew Array is ";
+         for(i=0;i<j;i++)
+             cout<<b[i]<<" ";
+     }
  
-    if(j==n)
-    {
-        cout<<"\nSoory!!!Element is not in the Array";
-        exit(0);
-    }
-    else
-    {
-        cout<<"\nNew Array is ";
-        for(i=0;i<j;i++)
-            cout<<b[i]<<" ";
-    }
- 
-    return 0;
+     return 0;
+   
 }
