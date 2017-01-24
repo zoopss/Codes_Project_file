8f901f498b0f3b692f2b236b61756e2f2aa9015d
Pranjal Kulshreshth <zoopss@users.noreply.github.com>
2017-01-24 08:46:39 +0530

    another one

--- codes/code10.cc
+++ codes/code10.cc
@@ -1,1 +1,22 @@
-
+#include<iostream.h>
+#include<string.h>
+class Salesman{   char name[26];
+                  float total_sales;
+               public:
+                  Salesman(char *s, Float f)
+                  {   strcpy(name," ");
+                      strcpy(name,s);
+                      total_sales = f;
+                  }
+                  void prnobject(void)
+                  {    cout.write(this->name, 26);
+                       cout<<"has invoked prnobject(). \n";
+                  }
+              };
+int main()
+{   Salesman Raman("Raman",21450), Sita("Sita",23190),VEdant("Vedant",19142);
+    Raman.prnobject();
+    Vedant.prnobject();
+    Sita.prnobject();
+    return 0;
+}
