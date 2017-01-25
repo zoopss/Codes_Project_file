5d3926b68c57983e73c35eabd51de70e56ffe544
Pranjal Kulshreshth <zoopss@users.noreply.github.com>
2017-01-25 09:21:44 +0530

    Update code14.cc

--- codes/code14.cc
+++ codes/code14.cc
@@ -1,1 +1,23 @@
-
+#include<iostream.h>
+#include<fstream.h>
+#include<string.h>
+#include<stdlib.h>
+struct customer
+{
+  char name[51];
+  float balance;
+};
+int main()
+{
+  System("cls");
+  customer savac;
+  strcpy(savac.name, "Tina Marshal");
+  savac.balance = 21310.75;
+  ofstream fout;
+  fout.open("Saving", ios :: out|ios::binary);
+  if(!fout)
+  {
+    cout<<"File cant be opened \n";
+    return 1;
+  }
+  fout.write
